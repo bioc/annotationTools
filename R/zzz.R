925eeb6eda651a71db5051beadc5ee87ba21c82c
Patrick Aboyoun <unknown>
2010-04-24 20:44:53 +0000

    Added a NAMESPACE file.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotationTools@46522 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .First.lib <- function(lib, pkg) {
-if(.Platform$OS.type == "windows" && require(Biobase) && interactive()  && .Platform$GUI == "Rgui") {
+if(.Platform$OS.type == "windows" && interactive()  && .Platform$GUI == "Rgui") {
 	addVigs2WinMenu("annotationTools")
 	}
 
